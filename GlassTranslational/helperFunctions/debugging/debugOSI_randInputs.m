5805bf30be762bf28e2da868dd0b17d6f0b11309
brittbushnell <31017677+brittbushnell@users.noreply.github.com>
2020-04-23 19:30:36 -0400

    Update debugOSI_randInputs.m

--- GlassTranslational/helperFunctions/debugging/debugOSI_randInputs.m
+++ GlassTranslational/helperFunctions/debugging/debugOSI_randInputs.m
@@ -3,7 +3,8 @@ dataT = data.LE;
 close all
 clc
 %%
-oris_rand = dataT.rotation;
+%oris_rand = dataT.rotation;
+oris_rand = 0:15:150;
 oris_randRadial = deg2rad(oris_rand);
 
 figure (1)
@@ -11,15 +12,15 @@ clf
 pos = get(gcf,'Position');
 set(gcf,'Position',[pos(1) pos(2) 1000 800])
 set(gcf,'PaperOrientation','Landscape');
-ttl = suptitle({'Distribtions of permuted OSI values using different random inputs';'blue: all ori resps = 20 red: only one ori = 40'});
+ttl = suptitle({'Distribtions of permuted OSI values using different random inputs orientations every 15deg';'blue: all ori resps = 20 red: only one ori = 40'});
 ttl.Position = [0.5,-0.025,0];
 %% noise near 0, flat ori tuning (all 20)
-resp_rand = rand(size(oris_rand));
+resp_rand = rand(size(dataT.rotation));
 or_rad = [0 pi/4 pi/2 pi*3/4];
 or_resp = [20 20 20 20];
 
 
-for or = 1:4
+for or = 1:(length(unique(or_rad)))
     dipole = resp_rand(randi(length(resp_rand),1)); % random val near zero
     stim = or_resp(or);
     baseSub = stim - dipole;
@@ -39,7 +40,7 @@ or_rad = [0 pi/4 pi/2 pi*3/4];
 or_resp = [0 0 0 40];
 
 
-for or = 1:4
+for or = 1:(length(unique(or_rad)))
     dipole = resp_rand(randi(length(resp_rand),1)); % random val near zero
     stim = or_resp(or);
     baseSub = stim - dipole;
@@ -55,16 +56,15 @@ v = sum(respVect);
 denom = sum((denomVect));
 SItmp1Ori = abs(v) / denom;
 %% test using values near zero 
-
 for nb = 1:2000
-    for or = 1:4
+    for or = 1:(length(unique(oris_rand)))
         %randTrial = randi(length(resp_randZero),1);
         
         dipole = resp_rand(randi(length(resp_rand),1));
         stim = resp_rand(randi(length(resp_rand),1));
         baseSub = stim - dipole;
         
-        ori2 = 2*(oris_randRadial(randi(length(resp_rand),1)));
+        ori2 = 2*(oris_randRadial(randi(length(oris_randRadial))));
         expon = 1i*(ori2);
         exVar = exp(expon);
         respVect(or,:) = baseSub*exVar;
@@ -94,20 +94,20 @@ set(gca,'tickdir','out','color','none','XTick',0:0.2:1)
 
 title('Random values between 0 and 1')
 %% test with 1/4 values negative
-numNeg = length(oris_rand)/4;
-negNdx = randi(length(oris_rand),[1,numNeg]);
+numNeg = length(resp_rand)/4;
+negNdx = randi(length(resp_rand),[1,numNeg]);
 resp_randNeg = resp_rand;
 resp_randNeg(negNdx) = resp_randNeg(negNdx) *-1;
 
 for nb = 1:2000
-    for or = 1:4
+    for or = 1:(length(unique(oris_rand)))
         %randTrial = randi(length(resp_randZero),1);
         
         dipole = resp_rand(randi(length(resp_randNeg),1));
         stim = resp_rand(randi(length(resp_randNeg),1));
         baseSub = stim - dipole;
         
-        ori2 = 2*(oris_randRadial(randi(length(resp_randNeg),1)));
+        ori2 = 2*(oris_randRadial(randi(length(oris_randRadial),1)));
         expon = 1i*(ori2);
         exVar = exp(expon);
         respVect(or,:) = baseSub*exVar;
@@ -137,17 +137,17 @@ set(gca,'tickdir','out','color','none','XTick',0:0.2:1)
 
 title('Random values between 0 and 1, 1/4 randomly negative')
 %% test with random values between 0 and 100
-resp_rand100 = randi(100,size(oris_rand));
+resp_rand100 = randi(100,size(resp_rand));
 
 for nb = 1:2000
-    for or = 1:4
+    for or = 1:(length(unique(oris_rand)))
         %randTrial = randi(length(resp_randZero),1);
         
         dipole = resp_rand100(randi(length(resp_rand100),1));
         stim = resp_rand100(randi(length(resp_rand100),1));
         baseSub = stim - dipole;
         
-        ori2 = 2*(oris_randRadial(randi(length(resp_rand100),1)));
+        ori2 = 2*(oris_randRadial(randi(length(oris_randRadial),1)));
         expon = 1i*(ori2);
         exVar = exp(expon);
         respVect(or,:) = baseSub*exVar;
@@ -177,20 +177,20 @@ set(gca,'tickdir','out','color','none','XTick',0:0.2:1)
 
 title('Random values between 0 and 100')
 %% test with 1/4 values 50:80, rest near 0
-numBig = length(oris_rand)/4;
-bigNdx = randi(length(oris_rand),[1,numBig]);
+numBig = length(resp_rand)/4;
+bigNdx = randi(length(resp_rand),[1,numBig]);
 resp_randBig = resp_rand;
 resp_randBig(negNdx) = resp_randBig(bigNdx) + (randi([50,80],1));
 
 for nb = 1:2000
-    for or = 1:4
+    for or = 1:(length(unique(oris_rand)))
         %randTrial = randi(length(resp_randZero),1);
         
         dipole = resp_rand(randi(length(resp_randBig),1));
         stim = resp_rand(randi(length(resp_randBig),1));
         baseSub = stim - dipole;
         
-        ori2 = 2*(oris_randRadial(randi(length(resp_randBig),1)));
+        ori2 = 2*(oris_randRadial(randi(length(oris_randRadial),1)));
         expon = 1i*(ori2);
         exVar = exp(expon);
         respVect(or,:) = baseSub*exVar;
