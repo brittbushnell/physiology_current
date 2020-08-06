21bcf7a560b2db3de9d214cc7d79d1c1ebfdf7ac
Brittany Bushnell <bushnell@zemina.cns.nyu.edu>
2020-08-06 11:00:07 -0400

    Zemina updates to rethresholding

--- reThresholdFiles.m
+++ reThresholdFiles.m
@@ -2,20 +2,55 @@ clear all
 close all
 clc
 tic
-%%
-files = {
-    'WU_RE_GratingsMapRF_nsp2_20170426_001';...
-    'WU_RE_GratingsMapRF_nsp2_20170815_001';...
-    'WU_RE_GratingsMapRF_nsp2_20170814_003';...
-    'WU_RE_GratingsMapRF_nsp2_20170814_002';...
-    'WU_RE_GratingsMapRF_nsp2_20170814_001';...
-    
-    'WU_RE_GratingsMapRF_nsp1_20170426_001';...
-    'WU_RE_GratingsMapRF_nsp1_20170815_001';...
-    'WU_RE_GratingsMapRF_nsp1_20170814_003';...
-    'WU_RE_GratingsMapRF_nsp1_20170814_002';...
-    'WU_RE_GratingsMapRF_nsp1_20170814_001';...
-};
+%%  Progress notes
 
-%threshold = 3.5;
-reThreshold(files);
+% WU nsp2 mapping - Laca crashed while running, so will need to complete it.
+
+
+
+%%
+animal = 'WV';
+mountMtx = {
+    'vnlstorage';
+    'vnlstorage2';
+    'vnlstorage3';
+            };
+        
+progType = 'TR'; % which file types do you want to run? Case doesn't matter in this scenario
+%%
+for mt = 1:length(mountMtx)
+    mount = mountMtx{mt};
+    for a = 1:2 % arrays
+        %%
+        if a == 1
+            array = 'nsp2';
+        else
+            array = 'nsp1';
+        end
+        dataDir = sprintf('/mnt/%s/bushnell_arrays/%s/%s_blackrock/%s/',mount,array,array,animal);
+        %cd '/km/vs/array/bushnell_arrays/nsp1/nsp1_blackrock/XT'
+        cd(dataDir)
+        %%
+        files = {};
+        tmpDir = dir;
+        ndx = 1;
+        for i = 1:length(tmpDir)
+            fname = tmpDir(i).name;
+            if contains(fname,progType,'IgnoreCase',true)
+                if contains(fname,'.nev') % doesn't matter which of the files, going to strip the end
+                    fname = strrep(fname,'.nev','');
+                    files{ndx} = fullfile(dataDir,fname);
+                    ndx = ndx+1;
+                end
+            end
+        end
+        %%
+        files(contains(files,'Bar')) = [];
+        files(contains(files,'test')) = [];
+        files(contains(files,'BE')) = [];
+        
+        %%
+        %threshold = 3.5;
+        reThreshold(files);
+    end
+end
