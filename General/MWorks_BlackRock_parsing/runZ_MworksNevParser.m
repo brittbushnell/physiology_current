231b26b04aecad813f928695f0a09aa6cddf5a76
brittbushnell <31017677+brittbushnell@users.noreply.github.com>
2020-08-28 16:42:50 -0400

    running on new  cleaned files

--- General/MWorks_BlackRock_parsing/runZ_MworksNevParser.m
+++ General/MWorks_BlackRock_parsing/runZ_MworksNevParser.m
@@ -22,10 +22,10 @@ tic;
 %% figure out what you're running
 monk = {
     'WV';
-%     'WU';
-%     'XT'
+    'WU';
+    'XT'
     };
-
+failNdx = 0;
 for mk = 1:length(monk)
     animal = monk{mk};
     
@@ -51,7 +51,7 @@ for mk = 1:length(monk)
                 end
                 %%
                 inputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/%s/',array,stimType,animal,eye);
-
+                
                 cd(inputDir);
                 tmp = dir;
                 ndx = 1;
@@ -60,58 +60,61 @@ for mk = 1:length(monk)
                 for t = 1:size(tmp,1)
                     if contains(tmp(t).name,'thresh')
                         if ~contains(tmp(t).name,'bar','IgnoreCase',true) % ignore bar map files
-                        files{ndx} = tmp(t).name;
-                        ndx = ndx+1;
+                            files{ndx} = tmp(t).name;
+                            ndx = ndx+1;
                         end
                     end
                 end
                 clear tmp
                 clear ndx
                 %%
-                failedFiles = [];
-                ndx = 1;
                 for fi = 1:size(files,2)
-                    try
-                        filename = string(files{fi});
-                        
-                        if contains(stimType,'png')
-                            if contains(filename,'map','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Mapping/%s/',array,stimType,animal,eye);
-                            elseif contains(filename,'glass','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Glass/%s/',array,stimType,animal,eye);
-                            elseif contains(filename,'freq','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/RadialFrequency/%s/',array,stimType,animal,eye);
-                            elseif contains(filename,'tex','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Textures/%s/',array,stimType,animal,eye);
-                            elseif contains(filename,'Pasupathy','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Pasupathy/%s/',array,stimType,animal,eye);
-                            else
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/%s/',array,stimType,animal,eye);
-                            end
+                     try
+                    filename = string(files{fi});
+                    
+                    if contains(stimType,'png')
+                        if contains(filename,'map','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Mapping/%s/',array,stimType,animal,eye);
+                        elseif contains(filename,'glass','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Glass/%s/',array,stimType,animal,eye);
+                        elseif contains(filename,'freq','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/RadialFrequency/%s/',array,stimType,animal,eye);
+                        elseif contains(filename,'tex','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Textures/%s/',array,stimType,animal,eye);
+                        elseif contains(filename,'Pasupathy','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Pasupathy/%s/',array,stimType,animal,eye);
+                        elseif contains(filename,'edge','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Edge/%s/',array,stimType,animal,eye);
                         else
-                            if contains(filename,'edge','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Edge/%s/',array,stimType,animal,eye);
-                            elseif contains (filename,'map','IgnoreCase',true)
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Mapping/%s/',array,stimType,animal,eye);
-                            else
-                                outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Gratings/%s/',array,stimType,animal,eye);
-                            end
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/%s/',array,stimType,animal,eye);
                         end
-                            
-                        fprintf('*** analyzing %s file %d/%d ****\n',filename,fi,size(files,2));
-                        if ~exist(sprintf('%s/%s',outputDir,strrep(filename,'.mat','')),'file')
+                    else
+                        if contains (filename,'map','IgnoreCase',true)
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Mapping/%s/',array,stimType,animal,eye);
+                        else
+                            outputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/parsed/Gratings/%s/',array,stimType,animal,eye);
+                        end
+                    end
+                    
+                    fprintf('*** analyzing %s file %d/%d ****\n',filename,fi,size(files,2));
+                    if ~exist(sprintf('%s/%s',outputDir,strrep(filename,'.mat','')),'file')
+                        if contains(filename,'__')
+                            continue
+                        else
                             MworksNevParser1(filename,10,100,outputDir);
                         end
-                        toc/3600;
-                        
-                    catch ME
-                        fprintf('\n\n%s failed %s\n\n',filename,ME.message);
-                        failedFiles{ndx} = filename;
-                        ndx = ndx+1;
                     end
+                    toc/3600;
+                    
+                    catch ME
+                        failNdx = failNdx+1;
+                        fprintf('\n\n%s failed %s\n\n',filename,ME.message);
+                        failedFiles{failNdx,1} = ME;
+                        
+                end
                 end
             end
         end
     end
 end
-fprintf('/n/n ***** %d FILES FAILED AT SOME POINT *****\n\n',ndx)
+fprintf('\n\n ***** %d FILES FAILED AT SOME POINT *****\n\n',failNdx)
--- Glass/Glass1_getInfo.m
+++ Glass/Glass1_getInfo.m
@@ -20,9 +20,8 @@ tic
 %%
 %cd ~/Dropbox/ArrayData/matFiles/reThreshold/png/WU/V4/Glass/LE/
 files = {
-    'WU_LE_GlassTR_nsp2_20170825_002_thresh35'
-    'WV_RE_glassCoh_nsp2_20190405_001_thresh35';
-    };
+'XT_RE_GlassTRCoh_nsp2_20190324_001_cleaned35ogcorrupt'
+};
 %%
 nameEnd = 'info';
 numPerm = 2000;
@@ -122,64 +121,90 @@ for fi = 1:size(files,1)
     end
     
     dataT.amap = aMap;
-    %% get receptive field parameters
-    % RF center is relative to fixation, not center of the monitor.
-    dataT = callReceptiveFieldParameters(dataT);
-    %% determine reponsive channels
-    dataT = GlassStimVsBlankPermutations_allStim(dataT,numPerm,holdout);
-    [dataT.stimBlankChPvals,dataT.responsiveCh] = glassGetPermutationStatsAndGoodCh(dataT.allStimBlankDprime,dataT.allStimBlankDprimeBootPerm);
-    fprintf('responsive channels defined\n')
-    %% find channels whose receptive fields are within the stimulus bounds
-    [dataT.rfQuadrant] = getRFsinStim(dataT);
-    dataT.inStim = ~isnan(dataT.rfQuadrant); % want all channels whos RF center is within the stimulus bounds to be 1.
-    dataT.goodCh = dataT.responsiveCh & dataT.inStim;
-    fprintf('%d good channels \n%d responsive channels\n',sum(dataT.responsiveCh), sum(dataT.goodCh))
-
-    %% get spike counts, Zscore, and split half correlations
-    if contains(dataT.programID,'TR')
-        [dataT.GlassTRSpikeCount,dataT.NoiseTRSpikeCount,dataT.BlankTRSpikeCount,dataT.AllStimTRSpikeCount] = getGlassTRSpikeCounts(dataT);
-        [dataT.GlassTRZscore,dataT.GlassAllStimTRZscore] = getGlassStimZscore(dataT);
-        [dataT.reliabilityIndex,dataT.splitHalfCorrBoots] = GlassTR_getHalfCorr(dataT);
-    else
-        [dataT.GlassSpikeCount,dataT.NoiseSpikeCount,dataT.BlankSpikeCount,dataT.AllStimSpikeCount] = getGlassCRSpikeCounts(dataT);
-        [dataT.GlassZscore,dataT.GlassAllStimZscore] = getGlassStimZscore(dataT);
-        [dataT.reliabilityIndex,dataT.split_half_correlation] = Glass_getHalfCorr(dataT);
+    
+    figure;
+    clf
+    pos = get(gcf,'Position');
+    set(gcf,'Position',[pos(1) pos(2) 1400 1200])
+    set(gcf,'PaperOrientation','Landscape');
+    for ch = 1:96
         
+        subplot(dataT.amap,10,10,ch)
+        hold on;
+        
+        REcoh = (dataT.coh == 1);
+        REnoiseCoh = (dataT.coh == 0);
+        REcohNdx = logical(REcoh + REnoiseCoh);
+        
+        blankResp = sum(smoothdata(dataT.bins((dataT.numDots == 0), 1:35 ,ch),'gaussian',3))./0.01;
+        stimResp = sum(smoothdata(dataT.bins((REcohNdx), 1:35 ,ch),'gaussian',3))./0.01;
+        plot(1:35,blankResp,'r','LineWidth',0.5);
+        plot(1:35,stimResp,'r','LineWidth',2);
+        title(ch)
+        set(gca,'Color','none','tickdir','out','FontAngle','italic','FontSize',10,'XTick',[],'YTick',[]);
+        ylim([0 inf])
     end
-    fprintf('spike counts done, zscores computed, halves correlated \n')
-    %% optional plots
-    if plotFlag == 1
-        if contains(dataT.programID,'TR')
-            plotGlassTR_spikeCounts(dataT)
-        else
-            
-        end
-    end
-    %% save data
-    
-    if location == 1
-        outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
-    elseif location == 0
-        outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
-    end
-    
-    if contains(filename,'LE')
-        data.LE = dataT;
-        data.RE = [];
-    else
-        data.RE = dataT;
-        data.LE = [];
-    end
-    
-    saveName = [outputDir filename '_' nameEnd '.mat'];
-    save(saveName,'data');
-    fprintf('%s saved\n', saveName)
-    %     catch ME
-    %         fprintf('%s did not work. \nError message: %s \n',filename,ME.message)
-    %         failedFiles{failNdx} = filename;
-    %         failedME{failNdx} = ME;
-    %         failNdx = failNdx+1;
-    %     end
+    suptitle({(sprintf('%s %s %s %s run %s', dataT.animal, dataT.array, dataT.programID,dataT.date,dataT.runNum));...
+        'clean data, Matlab parser'})
+    pause(0.1)
+    %% get receptive field parameters
+%     % RF center is relative to fixation, not center of the monitor.
+%     dataT = callReceptiveFieldParameters(dataT);
+%     %% determine reponsive channels
+%     dataT = GlassStimVsBlankPermutations_allStim(dataT,numPerm,holdout);
+%     [dataT.stimBlankChPvals,dataT.responsiveCh] = glassGetPermutationStatsAndGoodCh(dataT.allStimBlankDprime,dataT.allStimBlankDprimeBootPerm);
+%     fprintf('responsive channels defined\n')
+%     %% find channels whose receptive fields are within the stimulus bounds
+%     [dataT.rfQuadrant] = getRFsinStim(dataT);
+%     dataT.inStim = ~isnan(dataT.rfQuadrant); % want all channels whos RF center is within the stimulus bounds to be 1.
+%     dataT.goodCh = dataT.responsiveCh & dataT.inStim;
+%     fprintf('%d good channels \n%d responsive channels\n',sum(dataT.responsiveCh), sum(dataT.goodCh))
+% 
+%     %% get spike counts, Zscore, and split half correlations
+%     if contains(dataT.programID,'TR')
+%         [dataT.GlassTRSpikeCount,dataT.NoiseTRSpikeCount,dataT.BlankTRSpikeCount,dataT.AllStimTRSpikeCount] = getGlassTRSpikeCounts(dataT);
+%         [dataT.GlassTRZscore,dataT.GlassAllStimTRZscore] = getGlassStimZscore(dataT);
+%         [dataT.reliabilityIndex,dataT.splitHalfCorrBoots] = GlassTR_getHalfCorr(dataT);
+%     else
+%         [dataT.GlassSpikeCount,dataT.NoiseSpikeCount,dataT.BlankSpikeCount,dataT.AllStimSpikeCount] = getGlassCRSpikeCounts(dataT);
+%         [dataT.GlassZscore,dataT.GlassAllStimZscore] = getGlassStimZscore(dataT);
+%         [dataT.reliabilityIndex,dataT.split_half_correlation] = Glass_getHalfCorr(dataT);
+%         
+%     end
+%     fprintf('spike counts done, zscores computed, halves correlated \n')
+%     %% optional plots
+%     if plotFlag == 1
+%         if contains(dataT.programID,'TR')
+%             plotGlassTR_spikeCounts(dataT)
+%         else
+%             
+%         end
+%     end
+%     %% save data
+%     
+%     if location == 1
+%         outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
+%     elseif location == 0
+%         outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
+%     end
+%     
+%     if contains(filename,'LE')
+%         data.LE = dataT;
+%         data.RE = [];
+%     else
+%         data.RE = dataT;
+%         data.LE = [];
+%     end
+%     
+%     saveName = [outputDir filename '_' nameEnd '.mat'];
+%     save(saveName,'data');
+%     fprintf('%s saved\n', saveName)
+%     %     catch ME
+%     %         fprintf('%s did not work. \nError message: %s \n',filename,ME.message)
+%     %         failedFiles{failNdx} = filename;
+%     %         failedME{failNdx} = ME;
+%     %         failNdx = failNdx+1;
+%     %     end
 end
 failedFiles
 toc
