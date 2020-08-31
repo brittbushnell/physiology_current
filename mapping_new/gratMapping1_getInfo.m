fa2ded169a414028aa4148378ee51cb951e28be9
Brittany Bushnell <31017677+brittbushnell@users.noreply.github.com>
2020-08-31 15:38:05 -0400

    fixing directories

--- mapping_new/gratMapping1_getInfo.m
+++ mapping_new/gratMapping1_getInfo.m
@@ -4,8 +4,11 @@ close all
 clc
 %%
 files = {
-    'WU_RE_GratingsMapRF_nsp2_20170814_all_thresh35';
-    'WU_LE_GratingsMapRF_nsp2_20170620_001_thresh35';     
+    'WU_RE_GratingsMapRF_nsp2_20170814_002_thresh35';
+    'WU_LE_GratingsMapRF_nsp2_20170814_003_thresh35';    
+    
+    'WU_RE_GratingsMapRF_nsp1_20170814_002_thresh35';
+    'WU_LE_GratingsMapRF_nsp1_20170814_003_thresh35';
      };
 nameEnd ='info';
 %%
@@ -45,9 +48,9 @@ for fi = 1:size(files,1)
     end
         %%
     if location == 1
-        outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/GratMapRF/',dataT.array);
+        outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/GratMapRF/info/',dataT.array);
     elseif location == 0
-        outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/GratMapRF/',dataT.array);
+        outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/GratMapRF/info/',dataT.array);
     end
        %% add stimulus center for other programs
     if contains(filename,'WU')
