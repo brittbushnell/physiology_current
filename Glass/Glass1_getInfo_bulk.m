6f55221898f7fea0c1611562d5a457e32d660786
Brittany Bushnell <31017677+brittbushnell@users.noreply.github.com>
2020-08-31 21:31:59 -0400

    Update Glass1_getInfo_bulk.m

--- Glass/Glass1_getInfo_bulk.m
+++ Glass/Glass1_getInfo_bulk.m
@@ -165,13 +165,13 @@ for an = 1:3
                     dataT.amap = aMap;
                     %% plot stim vs blank PSTH to look for timing funkiness
                     if location == 0
-                        figDir =  sprintf( '/Users/brittany/Dropbox/Figures/%s/GlassTR/%s/PSTH/%s/',dataT.animal, dataT.array,eye);
+                        figDir =  sprintf( '/Users/brittany/Dropbox/Figures/%s/GlassTR/%s/PSTH/%s/stimVblank',dataT.animal, dataT.array,eye);
                     else
-                        figDir =  sprintf( '/Local/Users/bushnell/Dropbox/Figures/%s/GlassTR/%s/PSTH/%s/',dataT.animal, dataT.array,eye);
+                        figDir =  sprintf( '/Local/Users/bushnell/Dropbox/Figures/%s/GlassTR/%s/PSTH/%s/stimVblank',dataT.animal, dataT.array,eye);
                     end
                     cd(figDir)
                     
-                    figure(200);
+                    figure%(200);
                     clf
                     pos = get(gcf,'Position');
                     set(gcf,'Position',[pos(1) pos(2) 1400 1200])
@@ -185,8 +185,8 @@ for an = 1:3
                         REnoiseCoh = (dataT.coh == 0);
                         REcohNdx = logical(REcoh + REnoiseCoh);
                         
-                        blankResp = sum(smoothdata(dataT.bins((dataT.numDots == 0), 1:35 ,ch),'gaussian',3))./0.01;
-                        stimResp = sum(smoothdata(dataT.bins((REcohNdx), 1:35 ,ch),'gaussian',3))./0.01;
+                        blankResp = sum(smoothdata(dataT.bins((dataT.numDots == 0), 1:35 ,ch),'gaussian',3));
+                        stimResp = sum(smoothdata(dataT.bins((REcohNdx), 1:35 ,ch),'gaussian',3));
                         plot(1:35,blankResp,'r','LineWidth',0.5);
                         plot(1:35,stimResp,'r','LineWidth',2);
                         title(ch)
@@ -196,58 +196,58 @@ for an = 1:3
                     suptitle({(sprintf('%s %s %s %s run %s', dataT.animal, dataT.array, dataT.programID,dataT.date,dataT.runNum));...
                         'clean data, Matlab parser'})
                     
-                    figName = [dataT.animal,'_',dataT.eye,'_',dataT.array,'_PSTH_',dataT.date2,'_',dataT.runNum,'.pdf'];    
-                    print(gcf, figName,'-dpdf','-fillpage')
+                    figName = [dataT.animal,'_',dataT.eye,'_',dataT.array,'_',dataT.programID,'_PSTH_',dataT.date2,'_',dataT.runNum,'.pdf'];    
+                     print(gcf, figName,'-dpdf','-fillpage')
                     %% get receptive field parameters
-                    % RF center is relative to fixation, not center of the monitor.
-                    dataT = callReceptiveFieldParameters(dataT);
-                    %% determine reponsive channels
-                    dataT = GlassStimVsBlankPermutations_allStim(dataT,numPerm,holdout);
-                    [dataT.stimBlankChPvals,dataT.responsiveCh] = glassGetPermutationStatsAndGoodCh(dataT.allStimBlankDprime,dataT.allStimBlankDprimeBootPerm);
-                    fprintf('responsive channels defined\n')
-                    %% find channels whose receptive fields are within the stimulus bounds
-                    [dataT.rfQuadrant] = getRFsinStim(dataT);
-                    dataT.inStim = ~isnan(dataT.rfQuadrant); % want all channels whos RF center is within the stimulus bounds to be 1.
-                    dataT.goodCh = dataT.responsiveCh & dataT.inStim;
-                    fprintf('%d good channels \n%d responsive channels\n',sum(dataT.responsiveCh), sum(dataT.goodCh))
-                    %% get spike counts, Zscore, and split half correlations
-                    if contains(dataT.programID,'TR')
-                        [dataT.GlassTRSpikeCount,dataT.NoiseTRSpikeCount,dataT.BlankTRSpikeCount,dataT.AllStimTRSpikeCount] = getGlassTRSpikeCounts(dataT);
-                        [dataT.GlassTRZscore,dataT.GlassAllStimTRZscore] = getGlassStimZscore(dataT);
-                        [dataT.reliabilityIndex,dataT.splitHalfCorrBoots] = GlassTR_getHalfCorr(dataT);
-                    else
-                        [dataT.GlassSpikeCount,dataT.NoiseSpikeCount,dataT.BlankSpikeCount,dataT.AllStimSpikeCount] = getGlassCRSpikeCounts(dataT);
-                        [dataT.GlassZscore,dataT.GlassAllStimZscore] = getGlassStimZscore(dataT);
-                        [dataT.reliabilityIndex,dataT.split_half_correlation] = Glass_getHalfCorr(dataT);
-                    end
-                    fprintf('spike counts done, zscores computed, halves correlated \n')
-                    %% optional plots
-                    if plotFlag == 1
-                        if contains(dataT.programID,'TR')
-                            plotGlassTR_spikeCounts(dataT)
-                        else
-                            
-                        end
-                    end
-                    %% save data
-                    
-                    if location == 1
-                        outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
-                    elseif location == 0
-                        outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
-                    end
-                    
-                    if contains(filename,'LE')
-                        data.LE = dataT;
-                        data.RE = [];
-                    else
-                        data.RE = dataT;
-                        data.LE = [];
-                    end
-                    
-                    saveName = [outputDir filename '_' nameEnd '.mat'];
-                    save(saveName,'data');
-                    fprintf('%s saved\n  run time: %.2f minutes', saveName, toc/60)
+%                     % RF center is relative to fixation, not center of the monitor.
+%                     dataT = callReceptiveFieldParameters(dataT);
+%                     %% determine reponsive channels
+%                     dataT = GlassStimVsBlankPermutations_allStim(dataT,numPerm,holdout);
+%                     [dataT.stimBlankChPvals,dataT.responsiveCh] = glassGetPermutationStatsAndGoodCh(dataT.allStimBlankDprime,dataT.allStimBlankDprimeBootPerm);
+%                     fprintf('responsive channels defined\n')
+%                     %% find channels whose receptive fields are within the stimulus bounds
+%                     [dataT.rfQuadrant] = getRFsinStim(dataT);
+%                     dataT.inStim = ~isnan(dataT.rfQuadrant); % want all channels whos RF center is within the stimulus bounds to be 1.
+%                     dataT.goodCh = dataT.responsiveCh & dataT.inStim;
+%                     fprintf('%d good channels \n%d responsive channels\n',sum(dataT.responsiveCh), sum(dataT.goodCh))
+%                     %% get spike counts, Zscore, and split half correlations
+%                     if contains(dataT.programID,'TR')
+%                         [dataT.GlassTRSpikeCount,dataT.NoiseTRSpikeCount,dataT.BlankTRSpikeCount,dataT.AllStimTRSpikeCount] = getGlassTRSpikeCounts(dataT);
+%                         [dataT.GlassTRZscore,dataT.GlassAllStimTRZscore] = getGlassStimZscore(dataT);
+%                         [dataT.reliabilityIndex,dataT.splitHalfCorrBoots] = GlassTR_getHalfCorr(dataT);
+%                     else
+%                         [dataT.GlassSpikeCount,dataT.NoiseSpikeCount,dataT.BlankSpikeCount,dataT.AllStimSpikeCount] = getGlassCRSpikeCounts(dataT);
+%                         [dataT.GlassZscore,dataT.GlassAllStimZscore] = getGlassStimZscore(dataT);
+%                         [dataT.reliabilityIndex,dataT.split_half_correlation] = Glass_getHalfCorr(dataT);
+%                     end
+%                     fprintf('spike counts done, zscores computed, halves correlated \n')
+%                     %% optional plots
+%                     if plotFlag == 1
+%                         if contains(dataT.programID,'TR')
+%                             plotGlassTR_spikeCounts(dataT)
+%                         else
+%                             
+%                         end
+%                     end
+%                     %% save data
+%                     
+%                     if location == 1
+%                         outputDir =  sprintf('~/bushnell-local/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
+%                     elseif location == 0
+%                         outputDir =  sprintf('~/Dropbox/ArrayData/matFiles/%s/Glass/info/',dataT.array);
+%                     end
+%                     
+%                     if contains(filename,'LE')
+%                         data.LE = dataT;
+%                         data.RE = [];
+%                     else
+%                         data.RE = dataT;
+%                         data.LE = [];
+%                     end
+%                     
+%                     saveName = [outputDir filename '_' nameEnd '.mat'];
+%                     save(saveName,'data');
+%                     fprintf('%s saved\n  run time: %.2f minutes', saveName, toc/60)
                 catch ME
                     fprintf('%s did not work. \nError message: %s \n',filename,ME.message)
                     failNdx = failNdx+1;
@@ -255,6 +255,7 @@ for an = 1:3
                     failedME{failNdx} = ME;
                     
                 end
+                clear dataT
             end
         end
     end
