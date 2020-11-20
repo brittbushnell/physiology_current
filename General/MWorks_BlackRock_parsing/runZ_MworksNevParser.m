25fd8a76b8adcca3d0d97f40d2e7adc519e7b97e
brittbushnell <31017677+brittbushnell@users.noreply.github.com>
2020-11-20 15:09:58 -0500

    Update runZ_MworksNevParser.m

--- General/MWorks_BlackRock_parsing/runZ_MworksNevParser.m
+++ General/MWorks_BlackRock_parsing/runZ_MworksNevParser.m
@@ -3,85 +3,70 @@ clear all
 close all
 tic;
 %%
-% addpath(genpath('~/Desktop/my_vnlstorage/bushnell_arrays/'))
-% addpath(genpath('~/Desktop/my_zemina/vnlstorage2/'));
-% addpath(genpath('~/Desktop/my_zemina/vnlstorage3/'));
-% addpath(genpath('/v/awake/'));
-% addpath('~/Desktop/my_vnlstorage/bushnell_arrays/nsp1/mworks/WU/')
-% addpath('~/Desktop/my_vnlstorage/bushnell_arrays/nsp1/mworks/XT/')
-% addpath('~/Desktop/my_vnlstorage/bushnell_arrays/nsp1/mworks/WV/')
-%
-% addpath('~/Desktop/my_zemina/vnlstorage2/bushnell_arrays/nsp1/mworks/WV/')
-% addpath('~/Desktop/my_zemina/vnlstorage2/bushnell_arrays/nsp1/mworks/XT/')
-%
-% addpath('~/Desktop/my_zemina/vnlstorage3/bushnell_arrays/nsp1/mworks/WV/')
 %%
-%animal = 'WU';
-%stimType = 'png';
-%stimType = 'grat';
-%% figure out what you're running
-% monks = {
-%      'WU';
-%     %'WV';
-%     %'XT';
-%     };
-% ez = {
-%     'LE';
-%     'RE';
-%     };
-% brArray = {
-%     'nsp2';
-%     'nsp1';
-%     };
-% %stimType = 'png';
-%  stimType = 'gratings';
-% %%
-% 
-% failNdx = 0;
-% 
-% ndx = 1;
-% files = {};
-% for an = 1:length(monks)
-%     animal = monks{an};
-%     for ey = 1:length(ez)
-%         eye = ez{ey};
-%         for ar = 1:length(brArray)
-%             array = brArray{ar};
-%             %%
-%             inputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/%s/',array,stimType,animal,eye);
-%             
-%             cd(inputDir);
-%             tmp = dir;
-%             %%
-%             for t = 1:size(tmp,1)
-%                 if contains(tmp(t).name,'thresh')
-%                     if ~contains(tmp(t).name,'bar','IgnoreCase',true)  % ignore bar map files
-%                         files{ndx} = tmp(t).name;
-%                         ndx = ndx+1;
-%                     end
-%                 end
-%             end
-%             clear tmp
-% %             clear ndx
-%         end
-%     end
-% end
-% 
-% if contains(stimType,'grat','IgnoreCase',true)
-%     files(contains(files,'Contour','IgnoreCase',true)) = [];
-% end
-%%
-files = {
-    'WU_LE_GratingsMapRF_nsp1_20170426_003_thresh35';
-    'WU_LE_GratingsMapRF_nsp1_20170814_003_thresh35';
+% figure out what you're running
+monks = {
+    %'WU';
+    %'WV';
+    'XT';
     };
-stimType = 'grat';
-array = 'nsp1';
-animal = 'WU';
-eye = 'LE';
+ez = {
+    'LE';
+    'RE';
+    };
+brArray = {
+    'nsp2';
+    'nsp1';
+    };
+stimType = 'png';
+% stimType = 'gratings';
+%%
+
+failNdx = 0;
+
+ndx = 1;
+files = {};
+for an = 1:length(monks)
+    animal = monks{an};
+    for ey = 1:length(ez)
+        eye = ez{ey};
+        for ar = 1:length(brArray)
+            array = brArray{ar};
+            %%
+            inputDir = sprintf('/users/bushnell/Desktop/my_zemina/vnlstorage3/bushnell_arrays/%s/reThreshold/%s/%s/%s/',array,stimType,animal,eye);
+            
+            cd(inputDir);
+            tmp = dir;
+            %%
+            for t = 1:size(tmp,1)
+                if contains(tmp(t).name,'thresh')
+                    if ~contains(tmp(t).name,'bar','IgnoreCase',true)  % ignore bar map files
+                        files{ndx} = tmp(t).name;
+                        ndx = ndx+1;
+                    end
+                end
+            end
+            clear tmp
+%             clear ndx
+        end
+    end
+end
+
+if contains(stimType,'grat','IgnoreCase',true)
+    files(contains(files,'Contour','IgnoreCase',true)) = [];
+end
+%%
+% files = {
+%     'WU_LE_GratingsMapRF_nsp1_20170426_003_thresh35';
+%     'WU_LE_GratingsMapRF_nsp1_20170814_003_thresh35';
+%     };
+% stimType = 'grat';
+% array = 'nsp1';
+% animal = 'WU';
+% eye = 'LE';
 %%
 for fi = 1:length(files)
-     %try
+    try
         filename = string(files{fi});
         
         if contains(stimType,'png')
@@ -118,13 +103,13 @@ for fi = 1:length(files)
         %end
         toc/3600;
         
-%     catch ME
-%         failNdx = failNdx+1;
-%         fprintf('\n\n****** %s failed %s ******\n\n',filename,ME.message);
-%         failedFiles{failNdx,1} = filename;
-%         failedFiles{failNdx,2} = ME.message;
-%         
-%     end
+    catch ME
+        failNdx = failNdx+1;
+        fprintf('\n\n****** %s failed %s ******\n\n',filename,ME.message);
+        failedFiles{failNdx,1} = filename;
+        failedFiles{failNdx,2} = ME.message;
+        
+    end
 end
 %         end
 %     end
