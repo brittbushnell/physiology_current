d27bc298e4c949f7c8bb220330b227d1a4a81a1b
Brittany Bushnell <31017677+brittbushnell@users.noreply.github.com>
2020-12-16 20:16:18 -0500

    scatter plot linear regressions, merging eyes in Glass

--- Glass/GlassCR/plotting/plotGlass_dPrimeScatter.m
+++ Glass/GlassCR/plotting/plotGlass_dPrimeScatter.m
@@ -1,5 +1,6 @@
 function [] = plotGlass_dPrimeScatter(data)
-
+% nullify any channels that are not to be inlcuded by changing them to
+% either nan or 0.
 for ch = 1:96
     REgch = data.RE.goodCh(1,ch);
     LEgch =  data.LE.goodCh(1,ch);
@@ -11,8 +12,7 @@ for ch = 1:96
         
         data.LE.radBlankDprime(end,:,:,ch) = nan(2,2);
         data.LE.conBlankDprime(end,:,:,ch) = nan(2,2);
-        data.LE.noiseBlankDprime(1,:,:,ch) = nan(2,2);
-        
+        data.LE.noiseBlankDprime(1,:,:,ch) = nan(2,2);    
     else
         if data.RE.goodCh(1,ch) == 0
             data.RE.radBlankDprime(end,:,:,ch) = zeros(2,2);
@@ -26,7 +26,9 @@ for ch = 1:96
         end
     end
 end
+
 %%
+
 figure(4)
 clf
 pos = get(gcf,'Position');
@@ -35,13 +37,17 @@ set(gcf,'PaperOrientation','Landscape');
 
 subplot(1,3,1)
 
-a = reshape(data.LE.radBlankDprime,1,numel(data.LE.radBlankDprime));
-b = reshape(data.RE.radBlankDprime,1,numel(data.RE.radBlankDprime));
+a = reshape(data.LE.radBlankDprime,numel(data.LE.radBlankDprime),1);
+b = reshape(data.RE.radBlankDprime,numel(data.RE.radBlankDprime),1);
+oneMtx = ones(length(b),1);
+regX = [oneMtx,b];
+[~,~,~,~,radReg] = regress(a,regX);
 
 hold on
 plot(a,b,'o','markerfacecolor',[0 0.6 0.2],'markeredgecolor','w','MarkerSize',7)
-
+text(-0.5, 4.5,sprintf('R2: %.3f',radReg(1))) 
 plot([-2 5],[-2 5],'k')
+
 title('Radial')
 set(gca,'color','none','tickdir','out','box','off','FontSize',12,'FontWeight','bold','FontAngle','italic',...
     'layer','top','XTick',0:2:4,'YTick',0:2:4);
@@ -58,12 +64,17 @@ end
 
 
 subplot(1,3,2)
-a = reshape(data.LE.conBlankDprime,1,numel(data.LE.conBlankDprime));
-b = reshape(data.RE.conBlankDprime,1,numel(data.RE.conBlankDprime));
+a = reshape(data.LE.conBlankDprime,numel(data.LE.conBlankDprime),1);
+b = reshape(data.RE.conBlankDprime,numel(data.RE.conBlankDprime),1);
+oneMtx = ones(length(b),1);
+regX = [oneMtx,b];
+[~,~,~,~,conReg] = regress(a,regX);
 
 hold on
 plot(a,b,'o','markerfacecolor',[0.7 0 0.7],'markeredgecolor','w','MarkerSize',7)
 plot([-2 5],[-2 5],'k')
+text(-0.5, 4.5,sprintf('R2: %.3f',conReg(1)))
+
 title('Concentric')
 set(gca,'color','none','tickdir','out','box','off','FontSize',12,'FontWeight','bold','FontAngle','italic',...
     'layer','top','XTick',0:2:4,'YTick',0:2:4);
@@ -73,12 +84,17 @@ axis square
 
 
 subplot(1,3,3)
-a = reshape(data.LE.noiseBlankDprime,1,numel(data.LE.noiseBlankDprime));
-b = reshape(data.RE.noiseBlankDprime,1,numel(data.RE.noiseBlankDprime));
+a = reshape(data.LE.noiseBlankDprime,numel(data.LE.noiseBlankDprime),1);
+b = reshape(data.RE.noiseBlankDprime,numel(data.RE.noiseBlankDprime),1);
+oneMtx = ones(length(b),1);
+regX = [oneMtx,b];
+[~,~,~,~,nosReg] = regress(a,regX);
 
 hold on
 plot(a,b,'o','markerfacecolor',[1 0.5 0.1],'markeredgecolor','w','MarkerSize',7)
 plot([-2 5],[-2 5],'k')
+text(-0.5, 4.5,sprintf('R2: %.3f',nosReg(1)))
+
 title('Noise')
 set(gca,'color','none','tickdir','out','box','off','FontSize',12,'FontWeight','bold','FontAngle','italic',...
     'XTick',0:2:4,'YTick',0:2:4);
@@ -89,19 +105,12 @@ axis square
 suptitle(sprintf('%s %s %s dPrimes for all dot, dx, coherence combinations',data.RE.animal, data.RE.programID, data.RE.array))
 %%
 location = determineComputer;
-if length(data.RE.inStim) > 96
-    if location == 1
-        figDir =  sprintf('~/bushnell-local/Dropbox/Figures/%s/%s/%s/dPrime/singleSession/',data.RE.animal,data.RE.programID,data.RE.array);
-    elseif location == 0
-        figDir =  sprintf('~/Dropbox/Figures/%s/%s/%s/dPrime/singleSession/',data.RE.animal,data.RE.programID,data.RE.array);
-    end
-else
-    if location == 1
-        figDir =  sprintf('~/bushnell-local/Dropbox/Figures/%s/%s/%s/dPrime/',data.RE.animal,data.RE.programID,data.RE.array);
-    elseif location == 0
-        figDir =  sprintf('~/Dropbox/Figures/%s/%s/%s/dPrime/',data.RE.animal,data.RE.programID,data.RE.array);
-    end
+if location == 1
+    figDir =  sprintf('~/bushnell-local/Dropbox/Figures/%s/%s/%s/dPrime/',data.RE.animal,data.RE.programID,data.RE.array);
+elseif location == 0
+    figDir =  sprintf('~/Dropbox/Figures/%s/%s/%s/dPrime/',data.RE.animal,data.RE.programID,data.RE.array);
 end
+
 if~exist(figDir,'dir')
     mkdir(figDir)
 end
